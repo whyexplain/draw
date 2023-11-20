835e09bcc4f36ad64086091dbdf024eaf0023237
whyexplain <whyexplainplease@gmail.com>
2023-11-19 21:44:11 -0800

    commit:	modified:   index.html commit:	modified:   javascript.js commit:	modified:   styles.css

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+const container = document.querySelector('#container')
+;
+
+for (let i = 0; i < 400; i++) {
+    console.log('BRUH');
+            var cell = document.createElement('div');
+            cell.classList.add('cell')
+            cell.style.cssText = 'height: 20px; width: 20px;'
+            container.appendChild(cell);
+}   
+
+
+
+console.log('lol')
