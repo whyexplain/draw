81105f6b8eb9bada9e685955a4acf431171bb508
whyexplain <whyexplainplease@gmail.com>
2023-11-20 15:09:38 -0800

    commit:	modified:   index.html commit:	modified:   javascript.js commit: modified:   styles.css

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+let blackA = false
+let eraserA = false
+
+
+
+
+
 
 const container = document.querySelector('#container')
 ;
@@ -5,10 +12,28 @@ const container = document.querySelector('#container')
 for (let i = 0; i < 400; i++) {
     console.log('BRUH');
             var cell = document.createElement('div');
-            cell.classList.add('cell')
+            cell.classList.add('cellA');
+            cell.setAttribute("id", 'cell');
             cell.style.cssText = 'height: 20px; width: 20px;'
             container.appendChild(cell);
+
+
+            cell.addEventListener("mouseenter", function( event ) {
+                if (blackA === true) {black()}
+                else if (eraserA === true) {eraser()}
+            }, false); 
+            
+            
 }   
+
+
+function black () {
+    event.target.style.backgroundColor = "black";
+}
+
+function eraser () {
+    event.target.style.backgroundColor = "white";
+}
 
 
 
